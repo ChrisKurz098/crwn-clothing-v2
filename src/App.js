1bd2d1c17c4a3dffbd6ccf03ee600fdc24c1dfd8
CKURZ <ckurz098@gmail.com>
2022-12-13 12:41:36 -0800

    Installed firebase for DB and authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavigationBar from "./routes/NavigationBar/NavigationBar.component";
 import Home from "./routes/Home/Home.components";
+import SignIn from "./routes/SignIn/SignIn.component";
 import { Route, Routes } from "react-router";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<NavigationBar />}>
           <Route index element={<Home/>} />
+          <Route path="/signin" element={<SignIn/>} />
         </Route>
       </Routes>
 
--- src/routes/NavigationBar/NavigationBar.component.jsx
+++ src/routes/NavigationBar/NavigationBar.component.jsx
@@ -1,14 +1,25 @@
 import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import './navigation.styles.scss';
 
 const NavigationBar = () => {
     return (
         <>
             <nav className="navigation">
                 <Link className='logo-container' to='/'>
-                    <div>Logo</div>
+                    <CrownLogo />
                 </Link>
 
                 <div className="nav-links-container">
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                    <Link className='nav-link' to='/contact'>
+                        CONTACT
+                    </Link>
+                    <Link className='nav-link' to='/signin'>
+                        SIGN IN
+                    </Link>
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
